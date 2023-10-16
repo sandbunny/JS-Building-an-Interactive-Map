57b322c236d00c78a7e59d7445d4687c189f0443
Eduardo Puga <0470hdbutter@gmail.com>
2023-10-16 12:07:52 -0700

    map and why its not working debugging

--- public/app.js
+++ public/app.js
@@ -1,32 +1,50 @@
-//Note: You do NOT need to create an external JS file for this project despite any instructions you may see in Canvas. This file, app.js, is the only external JS file you need.
+// window.onload = async () => {
+//   let location = await getUserCoordinates();
+//   myMap.createMap(coordinates);
+// };
 
+const map = L.map("map"), {
+  center: coordinates,
+  zoom: 13,
+});
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 19,
+}).addTo(myMap);
 
+const marker = L.maker(coordinates);
+marker.addTo(map).bindPopup("<b>Hello world!").openPopup();
 
-// Create map
+//  async function getUserCoordinates() {
+//   let position = await new Promise((resolve, reject) => {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+//   return [position.coords.latitude, position.coords.longitude];
+// }
 
+// const options = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization: "fsq36S45VReQUTF/Vo7esrKY5uGXUOxtYze9kwmlGgcL10k=",
+//   },
+// };
 
+// async function fetchPlaces(business) {
+//   let respone = await fetch(
+//     `https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=coffee&limit=5&ll=41.8781%2C-87.6298`,
+//     options
+//   );
+//   let places = respone.json();
+//   return places;
+// }
 
-
-// Add openstreetmap tiles
-
-
-
-//Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
-
-
-// Create and add a geolocation marker
-
-
-
-
-// Draw the 2nd arrondissement
-
-
-
-
-// Metro station markers
-
-
-
-
-// Create red pin marker
+// function parseLocations() {
+//   let locations = [ ];
+//   results.forEach((result) => {
+//     let location = [results.geocodes.main, result.name];
+//     locations.push(location);
+//   });
+//   return locations;
+// }
